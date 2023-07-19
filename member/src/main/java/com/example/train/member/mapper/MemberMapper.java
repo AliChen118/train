c14830f97cc41024cb13979404e7b9b7142c46ce
Ali <1185462216@qq.com>
2023-07-19 23:39:43 +0800

    4.14 集成Mybatis官方生成器

--- member/src/main/java/com/example/train/member/mapper/MemberMapper.java
+++ member/src/main/java/com/example/train/member/mapper/MemberMapper.java
@@ -1,9 +1,31 @@
 package com.example.train.member.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.example.train.member.domain.Member;
+import com.example.train.member.domain.MemberExample;
+import org.apache.ibatis.annotations.Param;
 
-@Mapper
+import java.util.List;
+
 public interface MemberMapper {
+    long countByExample(MemberExample example);
 
-    int count();
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
 }
--- member/src/main/java/com/example/train/member/service/MemberService.java
+++ member/src/main/java/com/example/train/member/service/MemberService.java
@@ -16,6 +16,6 @@ public class MemberService {
     private MemberMapper memberMapper;
 
     public int count() {
-        return memberMapper.count();
+        return Math.toIntExact(memberMapper.countByExample(null));
     }
 }
