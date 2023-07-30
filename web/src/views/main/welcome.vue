108a81f21813d13e4b183d7f68385296b112a437
Ali <1185462216@qq.com>
2023-07-30 08:26:12 +0800

    7.6 增加welcome页面

---
+++ web/src/views/main/welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>欢迎使用12306网站</h1>
+</template>
+
+<script>
+import { defineComponent  } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+    };
+  },
+});
+</script>
+
+<style>
+</style>
