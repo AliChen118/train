a73c40dade1b178ade366bdc768aa2ca87eef62e
Ali <1185462216@qq.com>
2023-08-01 21:51:49 +0800

    8.5 使用PassangerService作为模板，生成service成功

--- generator/src/main/java/com/example/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/example/train/generator/server/ServerGenerator.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ServerGenerator {
+    static String servicePath = "[module]/src/main/java/com/example/train/[module]/service/";
     static String toPath = "generator\\src\\main\\java\\com\\example\\train\\generator\\test\\";
     static String pomPath = "generator\\pom.xml";
     static {
@@ -17,6 +18,43 @@ public class ServerGenerator {
     }
 
     public static void main(String[] args) throws Exception {
+        // 获取mybatis-generator
+        String generatorPath = getGeneratorPath();
+        // 比如generator-config-member.xml，得到module = member
+        String module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
+        System.out.println("module: " + module);
+        servicePath = servicePath.replace("[module]", module);
+        // new File(servicePath).mkdirs();
+        System.out.println("servicePath: " + servicePath);
+
+        // 读取table节点
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+        // 示例：表名 jiawa_test
+        // Domain = JiawaTest
+        String Domain = domainObjectName.getText();
+        // domain = jiawaTest
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        // do_main = jiawa-test
+        String do_main = tableName.getText().replaceAll("_", "-");
+
+        // 组装参数
+        Map<String, Object> param = new HashMap<>();
+        param.put("Domain", Domain);
+        param.put("domain", domain);
+        param.put("do_main", do_main);
+        System.out.println("组装参数：" + param);
+
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(servicePath + Domain + "Service.java", param);
+    }
+
+    public static void  printTableAndDomain() throws Exception{
         String generatorPath = getGeneratorPath();
 
         Document document = new SAXReader().read("generator/" + generatorPath);
