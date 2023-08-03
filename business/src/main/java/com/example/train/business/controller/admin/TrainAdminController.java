c57bf2f879e5cbdd23a946ba9191669af895a201
Ali <1185462216@qq.com>
2023-08-03 22:21:46 +0800

    9.12 火车车次下拉选择开发，增加后端接口

--- business/src/main/java/com/example/train/business/controller/admin/TrainAdminController.java
+++ business/src/main/java/com/example/train/business/controller/admin/TrainAdminController.java
@@ -1,14 +1,16 @@
 package com.example.train.business.controller.admin;
 
-import com.example.train.common.resp.CommonResp;
-import com.example.train.common.resp.PageResp;
 import com.example.train.business.req.TrainQueryReq;
 import com.example.train.business.req.TrainSaveReq;
 import com.example.train.business.resp.TrainQueryResp;
 import com.example.train.business.service.TrainService;
+import com.example.train.common.resp.CommonResp;
+import com.example.train.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -28,6 +30,12 @@ public class TrainAdminController {
     public CommonResp<PageResp<TrainQueryResp>> queryList(@Valid TrainQueryReq req) {
         PageResp<TrainQueryResp> pageResp = trainService.queryList(req);
         return new CommonResp<>(pageResp);
+    }
+
+    @GetMapping ("/query-all")
+    public CommonResp<List<TrainQueryResp>> queryAll() {
+        List<TrainQueryResp> list = trainService.queryAll();
+        return new CommonResp<>(list);
     }
 
     @DeleteMapping ("/delete/{id}")
