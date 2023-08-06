aa5885f4c1d7bc46aa09d7a8e77e7fb238392999
Ali <1185462216@qq.com>
2023-08-06 09:32:58 +0800

    9.16 实现按车次增加生成座位功能，增加后端接口，测试成功

--- business/src/main/java/com/example/train/business/controller/admin/TrainAdminController.java
+++ business/src/main/java/com/example/train/business/controller/admin/TrainAdminController.java
@@ -3,6 +3,7 @@ package com.example.train.business.controller.admin;
 import com.example.train.business.req.TrainQueryReq;
 import com.example.train.business.req.TrainSaveReq;
 import com.example.train.business.resp.TrainQueryResp;
+import com.example.train.business.service.TrainSeatService;
 import com.example.train.business.service.TrainService;
 import com.example.train.common.resp.CommonResp;
 import com.example.train.common.resp.PageResp;
@@ -19,6 +20,9 @@ public class TrainAdminController {
 
     @Resource
     private TrainService trainService;
+
+    @Resource
+    TrainSeatService trainSeatService;
 
     @PostMapping ("/save")
     public CommonResp<Integer> save(@Valid @RequestBody TrainSaveReq req) {
@@ -41,6 +45,12 @@ public class TrainAdminController {
     @DeleteMapping ("/delete/{id}")
     public CommonResp<Object> delete(@PathVariable Long id) {
         trainService.delete(id);
+        return new CommonResp<>();
+    }
+
+    @GetMapping  ("/gen-seat/{trainCode}")
+    public CommonResp<Object> genSeat(@PathVariable String trainCode) {
+        trainSeatService.genTrainSeat(trainCode);
         return new CommonResp<>();
     }
 }
--- business/src/main/java/com/example/train/business/service/TrainSeatService.java
+++ business/src/main/java/com/example/train/business/service/TrainSeatService.java
@@ -3,6 +3,9 @@ package com.example.train.business.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.example.train.business.domain.TrainCarriage;
+import com.example.train.business.enums.SeatColEnum;
 import com.example.train.common.resp.PageResp;
 import com.example.train.common.util.SnowUtil;
 import com.example.train.business.domain.TrainSeat;
@@ -17,6 +20,7 @@ import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -27,6 +31,9 @@ public class TrainSeatService {
 
     @Resource
     private TrainSeatMapper trainSeatMapper;
+    
+    @Resource
+    private TrainCarriageService trainCarriageService;
 
     public void save(TrainSeatSaveReq req) {
         DateTime now = new DateTime();
@@ -44,7 +51,7 @@ public class TrainSeatService {
 
     public PageResp<TrainSeatQueryResp> queryList(TrainSeatQueryReq req) {
         TrainSeatExample trainSeatExample = new TrainSeatExample();
-        trainSeatExample.setOrderByClause("train_code asc, carriage_index asc");
+        trainSeatExample.setOrderByClause("train_code asc, carriage_index asc, carriage_seat_index asc");
         TrainSeatExample.Criteria criteria = trainSeatExample.createCriteria();
         if (ObjectUtil.isNotEmpty(req.getTrainCode())) {
             criteria.andTrainCodeEqualTo(req.getTrainCode());
@@ -66,7 +73,64 @@ public class TrainSeatService {
         return pageResp;
     }
 
+    @Transactional
     public void delete(Long id) {
         trainSeatMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 批量生成火车座位
+     * @param trainCode
+     * 批量生成需要考虑是否支持重复生成：
+     *      1. 存在就跳过
+     *      2. 先删除已有的再生成 √
+     * 批量生成数据还得好绿的问题：【事务】
+     */
+    public void genTrainSeat(String trainCode) {
+        DateTime now = DateTime.now();
+        // 1. 清空当前火车下的所有座位
+        TrainSeatExample trainSeatExample = new TrainSeatExample();
+        TrainSeatExample.Criteria criteria = trainSeatExample.createCriteria();
+        criteria.andTrainCodeEqualTo(trainCode);
+        trainSeatMapper.deleteByExample(trainSeatExample);
+        // 2. 查询当前火车下的所有车厢
+        List<TrainCarriage> carriageList = trainCarriageService.selectByTrainCode(trainCode);
+        LOG.info("当前车次下的车厢数: {}", carriageList.size());
+
+        // 3. 循环生成每个车厢的座位
+        for (TrainCarriage trainCarriage : carriageList) {
+            // 3.1 拿到车厢数据，行数和座位类型(列数)
+            Integer rowCount = trainCarriage.getRowCount();
+            String seatType = trainCarriage.getSeatType();
+            int seatIndex = 1;
+
+            // 3.2 根据车厢类型，筛选出所有列，比如车厢类型是一等座，则筛选出columnList
+            List<SeatColEnum> colEnumList = SeatColEnum.getColsByType(seatType);
+            LOG.info("根据车厢的座位类型，筛选出所有列: {}", colEnumList);
+
+            // 3.3 循环行数 - 循环列数
+            for (int row = 1; row <= rowCount ; row++) {
+                for (SeatColEnum seatColEnum : colEnumList) {
+                    TrainSeat trainSeat = new TrainSeat();
+                    trainSeat.setId(SnowUtil.getSnowflakeNextId());
+                    trainSeat.setTrainCode(trainCode);
+                    trainSeat.setCarriageIndex(trainCarriage.getIndex());
+                    trainSeat.setRow(StrUtil.fillBefore(String.valueOf(row), '0', 2));
+                    trainSeat.setCol(seatColEnum.getCode());
+                    trainSeat.setSeatType(seatType);
+                    trainSeat.setCarriageSeatIndex(seatIndex++);
+                    trainSeat.setCreateTime(now);
+                    trainSeat.setUpdateTime(now);
+
+                    trainSeatMapper.insert(trainSeat);
+                    
+                }
+
+            }
+        }
+
+
+
+
     }
 }
