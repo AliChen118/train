d92adceb197e913298d82a96852c5451012b57da
Ali <1185462216@qq.com>
2023-08-06 18:57:36 +0800

    10.6 禁止任务并发执行

--- batch/src/main/java/com/example/train/batch/job/TestJob.java
+++ batch/src/main/java/com/example/train/batch/job/TestJob.java
@@ -1,5 +1,6 @@
 package com.example.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -9,10 +10,17 @@ import org.quartz.JobExecutionException;
  * @date 2023-08-06 18:41
  */
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob Test!");
+        System.out.println("TestJob Test开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob Test结束");
     }
 }
