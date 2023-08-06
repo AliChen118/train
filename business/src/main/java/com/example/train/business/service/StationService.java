8f6323977d3eda898b189d36b40f4b1a8f4bcc24
Ali <1185462216@qq.com>
2023-08-06 14:12:42 +0800

    9.18 保存车站之前，先校验唯一键是否存在

--- business/src/main/java/com/example/train/business/service/StationService.java
+++ business/src/main/java/com/example/train/business/service/StationService.java
@@ -1,6 +1,7 @@
 package com.example.train.business.service;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.example.train.business.domain.Station;
@@ -9,6 +10,8 @@ import com.example.train.business.mapper.StationMapper;
 import com.example.train.business.req.StationQueryReq;
 import com.example.train.business.req.StationSaveReq;
 import com.example.train.business.resp.StationQueryResp;
+import com.example.train.common.exception.BusinessException;
+import com.example.train.common.exception.BusinessExceptionEnum;
 import com.example.train.common.resp.PageResp;
 import com.example.train.common.util.SnowUtil;
 import com.github.pagehelper.PageHelper;
@@ -32,6 +35,14 @@ public class StationService {
         DateTime now = new DateTime();
         Station station = BeanUtil.copyProperties(req, Station.class);
         if (ObjectUtil.isNull(station.getId())) {
+
+            // 保存之前，先校验唯一键是否存在
+            StationExample stationExample = new StationExample();
+            stationExample.createCriteria().andNameEqualTo(req.getName());
+            List<Station> stationList = stationMapper.selectByExample(stationExample);
+            if (CollUtil.isNotEmpty(stationList)) {
+                throw new BusinessException(BusinessExceptionEnum.BUSINESS_STATION_NAME_UNIQUE_ERROR);
+            }
             station.setId(SnowUtil.getSnowflakeNextId());
             station.setCreateTime(now);
             station.setUpdateTime(now);
