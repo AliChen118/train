fb2f71a68df4a8e6021e12e89338328995a53edc
Ali <1185462216@qq.com>
2023-08-07 22:22:36 +0800

    11.6 修改每日座位页面，增加车次查询条件，修改列号的显示

--- business/src/main/java/com/example/train/business/req/DailyTrainSeatQueryReq.java
+++ business/src/main/java/com/example/train/business/req/DailyTrainSeatQueryReq.java
@@ -2,11 +2,34 @@ package com.example.train.business.req;
 
 import com.example.train.common.req.PageReq;
 
+import java.util.Date;
+
 public class DailyTrainSeatQueryReq extends PageReq {
+
+    private Date date;
+    private String trainCode;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
 
     @Override
     public String toString() {
         return "DailyTrainSeatQueryReq{" +
+                "date=" + date +
+                ", trainCode='" + trainCode + '\'' +
                 "} " + super.toString();
     }
 }
