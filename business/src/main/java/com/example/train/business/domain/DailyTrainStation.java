35594c00fd11843bf3c5117435b6cd1ba1de4ef7
Ali <1185462216@qq.com>
2023-08-07 08:42:32 +0800

    11.4 快速生成每日车站数据管理功能

--- business/src/main/java/com/example/train/business/domain/DailyTrainStation.java
+++ business/src/main/java/com/example/train/business/domain/DailyTrainStation.java
@@ -1,77 +1,31 @@
-package com.example.train.business.req;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+package com.example.train.business.domain;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class TrainStationSaveReq {
-
-    /**
-     * id
-     */
+public class DailyTrainStation {
     private Long id;
 
-    /**
-     * 车次编号
-     */
-    @NotBlank(message = "【车次编号】不能为空")
+    private Date date;
+
     private String trainCode;
 
-    /**
-     * 站序
-     */
-    @NotNull(message = "【站序】不能为空")
     private Integer index;
 
-    /**
-     * 站名
-     */
-    @NotBlank(message = "【站名】不能为空")
     private String name;
 
-    /**
-     * 站名拼音
-     */
-    @NotBlank(message = "【站名拼音】不能为空")
     private String namePinyin;
 
-    /**
-     * 进站时间
-     */
-    @JsonFormat(pattern = "HH:mm:ss",timezone = "GMT+8")
     private Date inTime;
 
-    /**
-     * 出站时间
-     */
-    @JsonFormat(pattern = "HH:mm:ss",timezone = "GMT+8")
     private Date outTime;
 
-    /**
-     * 停站时长
-     */
-    @JsonFormat(pattern = "HH:mm:ss",timezone = "GMT+8")
     private Date stopTime;
 
-    /**
-     * 里程（公里）|从上一站到本站的距离
-     */
-    @NotNull(message = "【里程（公里）】不能为空")
     private BigDecimal km;
 
-    /**
-     * 新增时间
-     */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
-    /**
-     * 修改时间
-     */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
@@ -80,6 +34,14 @@ public class TrainStationSaveReq {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 
     public String getTrainCode() {
@@ -169,6 +131,7 @@ public class TrainStationSaveReq {
         sb.append(" [");
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
+        sb.append(", date=").append(date);
         sb.append(", trainCode=").append(trainCode);
         sb.append(", index=").append(index);
         sb.append(", name=").append(name);
