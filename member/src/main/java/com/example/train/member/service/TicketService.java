69ef7e2e1f06c8f6b1e4dee6517947be184a15a0
Ali <1185462216@qq.com>
2023-08-12 20:06:04 +0800

    12.22 会员端增加【我的车票】页面

--- member/src/main/java/com/example/train/member/service/TicketService.java
+++ member/src/main/java/com/example/train/member/service/TicketService.java
@@ -2,6 +2,7 @@ package com.example.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjUtil;
 import com.example.train.common.req.MemberTicketReq;
 import com.example.train.common.resp.PageResp;
 import com.example.train.common.util.SnowUtil;
@@ -45,6 +46,9 @@ public class TicketService {
         TicketExample ticketExample = new TicketExample();
         ticketExample.setOrderByClause("id desc");
         TicketExample.Criteria criteria = ticketExample.createCriteria();
+        if (ObjUtil.isNotNull(req.getMemberId())) {
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        }
 
         LOG.info("查询页码: {}", req.getPage());
         LOG.info("每页条数: {}", req.getSize());
