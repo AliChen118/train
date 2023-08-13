c3558df9a8fe5a2fa09aeef1d61cfdc946551797
Ali <1185462216@qq.com>
2023-08-14 07:56:23 +0800

    13.4 读取配置文件的配置

---
+++ member/src/main/java/com/example/train/member/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.train.member.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ali
+ * @date 2023-08-14 07:50
+ */
+
+@RestController
+public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return String.format("Hello %s!", testNacos);
+    }
+}
