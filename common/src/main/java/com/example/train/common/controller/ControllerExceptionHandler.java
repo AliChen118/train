9a157c41c1eadf4a8bc6d78dcfc08d6d835c3774
Ali <1185462216@qq.com>
2023-08-16 22:40:45 +0800

    15.5 集成seata，分布式事务测试成功；seata-server 1.6.1 直接解压运行，不需要修改里面的配置 1. 生成undo.log 2. 引入依赖 3. bootStrap增加配置 4. 增加全局事务注解@GlobalTransactional

--- common/src/main/java/com/example/train/common/controller/ControllerExceptionHandler.java
+++ common/src/main/java/com/example/train/common/controller/ControllerExceptionHandler.java
@@ -6,8 +6,10 @@ package com.example.train.common.controller;
  */
 
 
+import cn.hutool.core.util.StrUtil;
 import com.example.train.common.exception.BusinessException;
 import com.example.train.common.resp.CommonResp;
+import io.seata.core.context.RootContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
@@ -31,7 +33,12 @@ public class ControllerExceptionHandler {
      */
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
-    public CommonResp exceptionHandler(Exception e) {
+    public CommonResp exceptionHandler(Exception e) throws Exception {
+        LOG.info("seata全局事务ID: {}", RootContext.getXID());
+        // 如果是在一次全局事务里出异常了，就不要包装返回值，将异常抛给调用方，让调用方回滚事务
+        if (StrUtil.isNotBlank(RootContext.getXID())) {
+            throw e;
+        }
         CommonResp commonResp = new CommonResp();
         LOG.error("系统异常：{}", e.getMessage());
         commonResp.setSuccess(false);
@@ -63,7 +70,7 @@ public class ControllerExceptionHandler {
     @ResponseBody
     public CommonResp exceptionHandler(BindException e) {
         CommonResp commonResp = new CommonResp();
-        LOG.error("业务异常: {}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        LOG.error("校验异常: {}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         commonResp.setSuccess(false);
         commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         return commonResp;
--- member/src/main/java/com/example/train/member/service/TicketService.java
+++ member/src/main/java/com/example/train/member/service/TicketService.java
@@ -13,6 +13,7 @@ import com.example.train.member.req.TicketQueryReq;
 import com.example.train.member.resp.TicketQueryResp;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import io.seata.core.context.RootContext;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,7 +34,8 @@ public class TicketService {
      *
      * @param req
      */
-    public void save(MemberTicketReq req) throws Exception {
+    public void save(MemberTicketReq req) {
+        LOG.info("seata全局事务ID save: {}", RootContext.getXID());
         DateTime now = DateTime.now();
         Ticket ticket = BeanUtil.copyProperties(req, Ticket.class);
         ticket.setId(SnowUtil.getSnowflakeNextId());
